cc382ae39b604e63aaede14f86e5f8b9b9b51b13
vaibhav019 <vaibhavsengarnetid@gmail.com>
2020-12-29 23:09:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+//import logo from "./logo.svg";
+import "./App.css";
 
+import MyCard from "./components/MyCard";
+import { getMatches } from "./api/api";
+import { Container, Grid } from "@material-ui/core";
 function App() {
+  const [matches, setMatches] = useState([]);
+
+  useEffect(() => {
+    getMatches()
+      .then((data) => {
+        console.log(data.matches);
+        setMatches(data.matches);
+      })
+      .catch((error) => {});
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Container>
+        <Grid container>
+          <Grid item xs={12}>
+          {
+            matches.map((match) => {
+            return (
+              <MyCard  match={match} />  
+            )
+            }
+              
+            )
+          }
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
-}
+            }
 
 export default App;
+
